94ce258c14a3d51a7d7b15e4e21904e4d57630df
Kul Paudel <kppaudel56@gmail.com>
2022-03-21 18:13:12 +0545

    DTIE_Tracker project started..

---
+++ src/test/java/com/online_dtie_tracker/OnlineDtieTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.online_dtie_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineDtieTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
