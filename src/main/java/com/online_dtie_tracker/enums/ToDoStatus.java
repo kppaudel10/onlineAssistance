014c015a6701a28813dc8f43fb92555d3d2078e7
Kul Paudel <kppaudel56@gmail.com>
2022-03-21 19:09:12 +0545

    add toDo model' '

---
+++ src/main/java/com/online_dtie_tracker/enums/ToDoStatus.java
@@ -0,0 +1,5 @@
+package com.online_dtie_tracker.enums;
+
+public enum ToDoStatus {
+    PENDING,DONE
+}
---
+++ src/main/java/com/online_dtie_tracker/model/ToDo.java
@@ -0,0 +1,10 @@
+package com.online_dtie_tracker.model;
+
+import com.online_dtie_tracker.enums.ToDoStatus;
+
+public class ToDo {
+    private Integer id;
+    private String title;
+    private ToDoStatus toDoStatus;
+    private User user;
+}
