8ab54cece35664119a6d342f6a48c4a5e984ced7
Kul Paudel <kppaudel56@gmail.com>
2022-03-21 19:05:27 +0545

    add userDto'

--- src/main/java/com/online_dtie_tracker/Dto/UserDto.java
+++ src/main/java/com/online_dtie_tracker/Dto/UserDto.java
@@ -1,4 +1,17 @@
 package com.online_dtie_tracker.Dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class UserDto {
+    private Integer id;
+    private String name;
+    private String address;
+    private String contact;
+    private String email;
+    private String password;
 }
