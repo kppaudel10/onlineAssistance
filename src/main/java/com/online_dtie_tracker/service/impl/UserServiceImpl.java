07d4412ea417ef0458c08f605c3b098066b7996e
Kul Paudel <kppaudel56@gmail.com>
2022-03-21 23:04:26 +0545

    add userService implementation

--- src/main/java/com/online_dtie_tracker/service/impl/UserServiceImpl.java
+++ src/main/java/com/online_dtie_tracker/service/impl/UserServiceImpl.java
@@ -1,28 +1,62 @@
 package com.online_dtie_tracker.service.impl;
 
 import com.online_dtie_tracker.Dto.UserDto;
+import com.online_dtie_tracker.conversion.DtoModelConvert;
+import com.online_dtie_tracker.model.User;
+import com.online_dtie_tracker.repo.user.UserRepo;
 import com.online_dtie_tracker.service.user.UserService;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
+    //first get user repository
+    private final UserRepo userRepo;
+
+    public UserServiceImpl(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
     @Override
     public UserDto save(UserDto userDto) throws IOException, ParseException {
-        return null;
+        //first convert Dto into Entity
+        User user = new DtoModelConvert().getUser(userDto);
+
+        //save into database
+       User user1 = userRepo.save(user);
+
+        //return userDto with id
+        return UserDto.builder().id(user1.getId()).build();
     }
 
     @Override
     public List<UserDto> findAll() {
-        return null;
+        return userRepo.findAll().stream().map(user -> {
+            return UserDto.builder()
+                    .id(user.getId())
+                    .name(user.getName())
+                    .address(user.getAddress())
+                    .contact(user.getContact())
+                    .email(user.getEmail()).build();
+        }).collect(Collectors.toList());
     }
 
     @Override
     public UserDto findById(Integer integer) throws IOException, ParseException {
-        return null;
+        //first find entity by id using repository
+      User user =  userRepo.findById(integer).get();
+
+      //return by making bto
+        return UserDto.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .address(user.getAddress())
+                .contact(user.getContact())
+                .email(user.getEmail()).build();
     }
 
     @Override
