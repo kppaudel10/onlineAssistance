929cb4204438a7cc8b61b98e18fbcbf80ae562cf
Kul Paudel <kppaudel56@gmail.com>
2022-03-22 15:06:01 +0545

    add login page

--- src/main/java/com/online_dtie_tracker/controller/MainController.java
+++ src/main/java/com/online_dtie_tracker/controller/MainController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("")
 public class MainController {
 
-    @GetMapping("/home")
+    @GetMapping("")
     public String getHomePage(){
         return "homepage/homepage";
     }
