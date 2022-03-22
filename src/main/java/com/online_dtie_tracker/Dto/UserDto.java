2748ece73986bfc385eba2de6a893cd6c018c82f
Kul Paudel <kppaudel56@gmail.com>
2022-03-22 17:39:48 +0545

    added signup form validation

--- src/main/java/com/online_dtie_tracker/Dto/UserDto.java
+++ src/main/java/com/online_dtie_tracker/Dto/UserDto.java
@@ -2,6 +2,11 @@ package com.online_dtie_tracker.Dto;
 
 import lombok.*;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -9,9 +14,26 @@ import lombok.*;
 @Builder
 public class UserDto {
     private Integer id;
+
+    @NotEmpty(message = "name must not be empty")
     private String name;
+
+    @NotEmpty(message = "address must not be empty")
     private String address;
+
+    @NotEmpty(message = "Mobile number must not be empty")
+    @Pattern(regexp = "^\\+?(?:977)?[ -]?(?:(?:(?:98|97)-?\\d{8})|(?:01-?\\d{7}))$",
+            message = "Invalid number.")
+    @Size(max = 10,min = 10 ,message = "Not valid length of number")
     private String contact;
+
+    @NotEmpty(message = "email must not be empty")
+    @Email(message = "enter valid email")
     private String email;
+
+    @NotEmpty(message = "password must not be empty")
     private String password;
+
+    @NotEmpty(message = "password must not be empty")
+    private String reTypePassword;
 }
--- src/main/java/com/online_dtie_tracker/controller/signup/SignUpController.java
+++ src/main/java/com/online_dtie_tracker/controller/signup/SignUpController.java
@@ -1,15 +1,59 @@
 package com.online_dtie_tracker.controller.signup;
 
+import com.online_dtie_tracker.Dto.UserDto;
+import com.online_dtie_tracker.conversion.DtoModelConvert;
+import com.online_dtie_tracker.service.impl.UserServiceImpl;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+import java.io.IOException;
+import java.text.ParseException;
 
 @Controller
 @RequestMapping("/signup")
 public class SignUpController {
+    //add userService for database
+    private final UserServiceImpl userService;
+
+    public SignUpController(UserServiceImpl userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/home")
-    public String getSignUpPage(){
+    public String getSignUpPage(Model model) {
+        model.addAttribute("userDto", new UserDto());
+        return "signuppage/signuppage";
+    }
+
+    @PostMapping("/add")
+    public String getSignUp(@Valid @ModelAttribute("userDto") UserDto userDto,
+                            BindingResult bindingResult, Model model) throws IOException, ParseException {
+        if (!bindingResult.hasErrors()) {
+            //first check user password is match or not
+            if (userDto.getPassword().equals(userDto.getReTypePassword())){
+                //save into database
+              UserDto userDto1  = userService.save(userDto);
+
+              if (userDto1 !=null){
+                  model.addAttribute("message","User Added successfully.");
+              }else {
+                  model.addAttribute("message", "Failed to add user");
+              }
+
+            }else {
+                //if user password isnot match
+                model.addAttribute("passwordMsg","password must match");
+            }
+        }
+
+        //if there is any error
+        model.addAttribute("userDto", userDto);
         return "signuppage/signuppage";
     }
 }
